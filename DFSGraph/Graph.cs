86bfd55a10d08a3ee92673d0de13545b0ea0e8cc
Farnoud Taghdissi Borhani <Farnood_t@yahoo.com>
2021-08-12 14:32:30 -0700

    Add project files.

---
+++ DFSGraph/Graph.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DFSGraph
+{
+    class Graph
+    {
+        int nodes;
+        List<List<int>> adj = new List<List<int>>();
+
+        public Graph(int V)
+        {
+            this.nodes = V;
+            for(int i = 0; i<V; i++)
+            {
+                adj.Add(new List<int>());
+            }
+        }
+
+        public void AddEdge(int Source, int Destination)
+        {
+            adj[Source].Add(Destination);
+        }
+
+        public void DFS(int StartNode)
+        {
+            bool[] Visited = new bool[nodes];
+            DFSUtil(StartNode, Visited);
+        }
+
+        public void DFSUtil(int StartNode, bool[] Visited)
+        {
+            Visited[StartNode] = true;
+            Console.Write(StartNode + " , ");
+            List<int> vNode = adj[StartNode];
+            foreach(var vchild in vNode)
+            {
+                if (!Visited[vchild])
+                    DFSUtil(vchild, Visited);
+            }
+        }
+    }
+}
---
+++ DFSGraph/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DFSGraph
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Graph G = new Graph(4);
+            G.AddEdge(0, 1);
+            G.AddEdge(0, 2);
+            G.AddEdge(1, 2);
+            G.AddEdge(2, 0);
+            G.AddEdge(2, 3);
+            G.AddEdge(3, 3);
+
+            G.DFS(2);
+            Console.ReadLine();
+        }
+    }
+}
